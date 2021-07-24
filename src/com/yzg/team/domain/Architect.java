2ab40e318d90265e77551e89c789947e12136425
Zg-Yao <1099856553@qq.com>
2021-07-24 18:39:15 +0800

    import java project

--- src/com/yzg/team/domain/Architect.java
+++ src/com/yzg/team/domain/Architect.java
@@ -1,8 +1,8 @@
 package com.yzg.team.domain;
 
-public class Architect extends Designer{
-	
-	private int stock;//表示公司奖励的股票数量
+public class Architect extends Designer {
+
+	private int stock;// 表示公司奖励的股票数量
 
 	public Architect() {
 		super();
@@ -20,16 +20,17 @@ public class Architect extends Designer{
 	public void setStock(int stock) {
 		this.stock = stock;
 	}
-//2	马化腾	32	18000.0	架构师	FREE	15000.0	2000	联想T4(6000.0)
+
+	// 2 马化腾 32 18000.0 架构师 FREE 15000.0 2000 联想T4(6000.0)
 	@Override
 	public String toString() {
-		return getDetails() + "\t架构师\t" + getStatus() + "\t" + getBonus() + "\t" + stock + "\t" + getEquipment().getDescription();
-	}
-	
-//	3/2     马化腾   32      18000.0  架构师   15000.0  2000
-	public String getMemmberOfInfPlus(){
-		return getMemmberOfInf() + "\t架构师 " + "\t" + getBonus() + "\t" + stock;
+		return getDetails() + "\t架构师\t" + getStatus() + "\t" + getBonus() + "\t" + stock + "\t"
+				+ getEquipment().getDescription();
 	}
 
+	// 3/2 马化腾 32 18000.0 架构师 15000.0 2000
+	public String getMemmberOfInfPlus() {
+		return getMemmberOfInf() + "\t架构师 " + "\t" + getBonus() + "\t" + stock;
+	}
 
 }
